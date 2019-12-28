a7ab6d7429e299e5528aa2f8af38d06346a71bef
davidluong98 <luongd5@student.lasalle.edu>
2019-12-28 00:21:35 -0500

    delete request added

--- routes/api/items.js
+++ routes/api/items.js
@@ -18,7 +18,15 @@ router.post("/", (req, res) => {
   const newItem = new Item({
     name: req.body.name
   });
-
   newItem.save().then(item => res.json(item));
 });
+
+//@route DELETE request to API/items
+//@desc delete an item
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
+});
+
 module.exports = router;

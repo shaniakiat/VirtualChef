e4473517fda1aca03ee4e402e86a65563aa8efd0
davidluong98 <luongd5@student.lasalle.edu>
2019-12-28 22:55:02 -0500

    Boilerplate-Static-Components, no CSS,

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
-      <h1>Hello, David!</h1>
+      <ShoppingList />
     </div>
   );
 }

ddf5d13fcdb64a9302a10a04af859c090cb6c585
shaniakiat <kiat.shania@gmail.com>
2019-12-29 18:52:56 -0500

    Styling Navbar + Logo

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import AppNavbar from "./components/AppNavbar";
 
+
+const leftItems = [
+  { as: "a", content: "Home", key: "home" },
+  { as: "a", content: "Users", key: "users" }
+];
+const rightItems = [
+  { as: "a", content: "Login", key: "login" },
+  { as: "a", content: "Register", key: "register" }
+];
+
 function App() {
   return (
     <div className="App">
-      <AppNavbar />
+      <AppNavbar leftItems={leftItems} rightItems={rightItems}>
+        {/* <Image src="https://react.semantic-ui.com/assets/images/wireframe/paragraph.png" /> */}
+      </AppNavbar>
       <h1>Hello, David!</h1>
     </div>
   );

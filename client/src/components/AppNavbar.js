b69557f5946b8dba011763397dab08101b780392
shaniakiat <kiat.shania@gmail.com>
2019-12-28 21:37:42 -0500

    AppNavbar component

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,49 @@
+import React, { Component, Fragment } from "react";
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from "reactstrap";
+
+class AppNavbar extends Component {
+  state = {
+    isOpen: false
+  };
+
+  // method
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/DavidLuong98/VirtualChef">
+                    VirtualChef
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;

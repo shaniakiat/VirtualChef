1f8a7c9097c36b4a64e8fef86ce4b4a8b27973f7
davidluong98 <luongd5@student.lasalle.edu>
2019-12-29 19:07:29 -0500

    started redux and created reducers

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,16 @@ import "./App.css";
 import AppNavbar from "./components/AppNavbar";
 import ShoppingList from "./components/ShoppingList";
 
+import { Provider } from "react-redux";
+import store from "./store";
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    </Provider>
   );
 }
 

ddd85e0f1898c012196a163502d809ae47d1aafb
shaniakiat <kiat.shania@gmail.com>
2019-12-29 19:00:40 -0500

    Fix Merge CONFLICT

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,23 @@ import "./App.css";
 import AppNavbar from "./components/AppNavbar";
 import ShoppingList from "./components/ShoppingList";
 
+
+const leftItems = [
+  { as: "a", content: "Home", key: "home" },
+  { as: "a", content: "Users", key: "users" }
+];
+const rightItems = [
+  { as: "a", content: "Login", key: "login" },
+  { as: "a", content: "Register", key: "register" }
+];
+
 function App() {
   return (
     <div className="App">
-      <AppNavbar />
+      <AppNavbar leftItems={leftItems} rightItems={rightItems}>
+        {/* <Image src="https://react.semantic-ui.com/assets/images/wireframe/paragraph.png" /> */}
+      </AppNavbar>
+      <h1>Hello, David!</h1>
       <ShoppingList />
     </div>
   );
--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,49 +1,108 @@
-import React, { Component, Fragment } from "react";
-
+import _ from "lodash";
+import React, { Component } from "react";
+import logo from "../logo-white.png"
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Container
-} from "reactstrap";
+  Container,
+  Icon,
+  Image,
+  Menu,
+  Sidebar,
+  Responsive
+} from "semantic-ui-react";
+
+const NavBarMobile = ({
+  children,
+  leftItems,
+  onPusherClick,
+  onToggle,
+  rightItems,
+  visible
+}) => (
+  <Sidebar.Pushable>
+    <Sidebar
+      as={Menu}
+      animation="overlay"
+      icon="labeled"
+      inverted
+      items={leftItems}
+      vertical
+      visible={visible}
+    />
+    <Sidebar.Pusher
+      dimmed={visible}
+      onClick={onPusherClick}
+      style={{ minHeight: "100vh" }}
+    >
+      <Menu fixed="top" inverted>
+        <Menu.Item>
+          <Image size="mini" src={logo} alt="logo"/>
+        </Menu.Item>
+        <Menu.Item onClick={onToggle}>
+          <Icon name="sidebar" />
+        </Menu.Item>
+        <Menu.Menu position="right">
+          {_.map(rightItems, item => <Menu.Item {...item} />)}
+        </Menu.Menu>
+      </Menu>
+      {children}
+    </Sidebar.Pusher>
+  </Sidebar.Pushable>
+);
+
+const NavBarDesktop = ({ leftItems, rightItems }) => (
+  <Menu fixed="top" inverted>
+    <Menu.Item>
+      <Image size="mini" src={logo} alt="logo"/>
+    </Menu.Item>
+      {_.map(leftItems, item => <Menu.Item {...item} />)}
+    <Menu.Menu position="right">
+      {_.map(rightItems, item => <Menu.Item {...item} />)}
+    </Menu.Menu>
+  </Menu>
+);
+
+const NavBarChildren = ({ children }) => (
+  <Container style={{ marginTop: "5em" }}>{children}</Container>
+);
 
 class AppNavbar extends Component {
   state = {
-    isOpen: false
+    visible: false
   };
 
-  // method
-  toggle = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
+  handlePusher = () => {
+    const { visible } = this.state;
+
+    if (visible) this.setState({ visible: false });
   };
+
+  handleToggle = () => this.setState({ visible: !this.state.visible });
 
   render() {
+    const { children, leftItems, rightItems } = this.props;
+    const { visible } = this.state;
+
     return (
       <div>
-        <Navbar color="dark" dark expand="sm" className="mb-5">
-          <Container>
-            <NavbarBrand href="/">ShoppingList</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <NavLink href="https://github.com/DavidLuong98/VirtualChef">
-                    VirtualChef
-                  </NavLink>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </Container>
-        </Navbar>
+        <Responsive {...Responsive.onlyMobile}>
+          <NavBarMobile
+            leftItems={leftItems}
+            onPusherClick={this.handlePusher}
+            onToggle={this.handleToggle}
+            rightItems={rightItems}
+            visible={visible}
+          >
+            <NavBarChildren>{children}</NavBarChildren>
+          </NavBarMobile>
+        </Responsive>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+          <NavBarDesktop leftItems={leftItems} rightItems={rightItems} />
+          <NavBarChildren>{children}</NavBarChildren>
+        </Responsive>
       </div>
     );
   }
 }
+
 
 export default AppNavbar;

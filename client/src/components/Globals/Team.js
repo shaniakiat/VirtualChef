32032eedcc31a810300b84f16e565718fbf2a933
shaniakiat <kiat.shania@gmail.com>
2020-01-09 13:29:00 -0500

    setting up team section

--- client/src/components/Globals/Team.js
+++ client/src/components/Globals/Team.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import David from "../../images/david-luong-pp.png";
+import Shania from "../../images/shania-kiat-pp.png";
+import Anthony from "../../images/anthony-ibe-pp.png";
 // import makeCarousel from "react-reveal/makeCarousel"
 // import Fade from "react-reveal/Fade"
 // import { FaLinkedin } from "react-icons/fa"
@@ -16,7 +19,19 @@ class Team extends Component {
             from various background.
             <div className="members">
               <div className="person">
-                <img src="im" alt="" />
+                <img src={David} alt="profile icon" />
+                <h3>David Luong</h3>
+                <p>Computer Science Student at La Salle University</p>
+              </div>
+              <div className="person">
+                <img src={Shania} alt="profile icon" />
+                <h3>Shania Kiat</h3>
+                <p>Computer Science Student at La Salle University</p>
+              </div>
+              <div className="person">
+                <img src={Anthony} alt="profile icon" />
+                <h3>Anthony Ibe</h3>
+                <p>Computer Science Student at La Salle University</p>
               </div>
             </div>
           </h1>

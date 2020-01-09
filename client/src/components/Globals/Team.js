0b97f813c2a7a8ebc1d63441546604cb5a32331f
shaniakiat <kiat.shania@gmail.com>
2020-01-09 13:03:56 -0500

    Team Section

---
+++ client/src/components/Globals/Team.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+// import makeCarousel from "react-reveal/makeCarousel"
+// import Fade from "react-reveal/Fade"
+// import { FaLinkedin } from "react-icons/fa"
+// import { FaGithub } from "react-icons/fa"
+// import styled from "styled-components"
+// import Resume from "../../../../static/resume.pdf"
+
+class Team extends Component {
+  render() {
+    return (
+      <section>
+        <div class="team">
+          <h1>
+            Our web application are crafted by our talented team of developers
+            from various background.
+            <div className="members">
+              <div className="person">
+                <img src="images/" alt="" />
+              </div>
+            </div>
+          </h1>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Team;

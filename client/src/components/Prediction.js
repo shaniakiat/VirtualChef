b1b135ef0c313d431645c078d8bb0187505a02fd
David Luong <davidluong@Davids-MBP.fios-router.home>
2020-02-01 19:43:12 -0500

    dispplay the predictions dynamically

--- client/src/components/Prediction.js
+++ client/src/components/Prediction.js
@@ -3,16 +3,15 @@ import React from "react";
 class Prediction extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { usersFood: "" };
+    this.state = { usersFood: "", domElementsObj: [] };
   }
 
   handleChange = event => {
     this.setState({ usersFood: event.target.value });
-    console.log(this.state.usersFood);
+    this.setState({ domElementsObj: [] });
   };
 
   getPredictions = () => {
-    alert(this.state.usersFood);
     let myObj = [];
     fetch(`http://localhost:8000/prediction/${this.state.usersFood}`, {
       headers: {
@@ -29,19 +28,45 @@ class Prediction extends React.Component {
 
         //getting individual words from the json file, now each element is formatted [{food, 0.9...}, {food2, 0.99}...]
         myObj = myJson;
+        this.setState({ domElementsObj: myJson });
         myObj.map(function(obj, i) {
-          if (i < 5) {
-            myObj[i] = obj[0];
-          }
+          myObj[i] = obj[0];
         });
         console.log(myObj);
-        // getIngredients(myObj);
+        //fetch the ingredients, but we wil change our approach
+        // this.getIngredients(myObj);
+        // this.createUI(myObj);
       })
       .catch(function(error) {
         console.error();
       }); //end of prediction fetch
   };
+  getIngredients = params => {
+    // https://developer.edamam.com/admin/applications/1409619036598?service_id=2555417725632
+    //example :https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free
+
+    //what we will do is make each predicted food a button, when the user click it - > they will be able to see the ingredient
+    console.log(params);
+    const base = "https://api.edamam.com/search";
+    const YOUR_APP_ID = "1379f77e";
+    const YOUR_APP_KEY = "5c98e2c53e8e6a98a17d608177ac37c1";
+    params.map(function(x, i) {
+      console.log(i);
+      fetch(
+        `${base}?q=${params[0]}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
+      )
+        .then(response => {
+          console.log(response.json());
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    });
+  };
   render() {
+    let predictionDOM = this.state.domElementsObj.map(i => {
+      return <li>{i}</li>;
+    });
     return (
       <div>
         <h1>testing api</h1>
@@ -53,6 +78,7 @@ class Prediction extends React.Component {
           placeholder="Enter your food"
         />
         <button onClick={this.getPredictions}>Make Predictions</button>
+        {predictionDOM}
       </div>
     );
   }

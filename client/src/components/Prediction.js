fa7810715ebe03b0380df007650d9e8edf3aa850
David Luong <davidluong@Davids-MBP.fios-router.home>
2020-02-02 22:26:07 -0500

    able to fetch the recipe obj

--- client/src/components/Prediction.js
+++ client/src/components/Prediction.js
@@ -3,7 +3,7 @@ import React from "react";
 class Prediction extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { usersFood: "", domElementsObj: [], foodTemp: [] };
+    this.state = { usersFood: "", domElementsObj: [] };
   }
 
   handleChange = event => {
@@ -23,8 +23,6 @@ class Prediction extends React.Component {
         return response.json();
       })
       .then(myJson => {
-        // console.log(myJson);
-        //getting individual words from the json file, now each element is formatted [{food, 0.9...}, {food2, 0.99}...]
         myObj = myJson;
         if (myObj == "Sorry, we couldn't indentify this food yet.") {
           this.setState({ domElementsObj: [] });
@@ -42,20 +40,26 @@ class Prediction extends React.Component {
   getIngredients = params => {
     // https://developer.edamam.com/admin/applications/1409619036598?service_id=2555417725632
     //example :https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free
-    console.log(params);
+    let ingredientObj = [];
     const base = "https://api.edamam.com/search";
     const YOUR_APP_ID = "1379f77e";
     const YOUR_APP_KEY = "5c98e2c53e8e6a98a17d608177ac37c1";
-    // params.map(function(x, i) {
-    console.log(params);
     fetch(`${base}?q=${params}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`)
-      .then(response => {
-        console.log(response.json());
+      .then(res => {
+        if (res.ok) {
+          // console.log(res.json());
+          return res.json();
+        } else {
+          throw new Error(res);
+        }
+      })
+      .then(resJson => {
+        ingredientObj = resJson;
+        console.log(ingredientObj);
       })
       .catch(function(error) {
         console.log(error);
       });
-    // });
   };
   render() {
     let predictionDOM = this.state.domElementsObj.map((obj, i) => {

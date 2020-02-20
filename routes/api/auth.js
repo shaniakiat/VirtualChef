36f27a271bc24cee01b77145c6824617763256d7
shaniakiat <kiat.shania@gmail.com>
2020-02-20 17:49:31 -0500

    fixing user id

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -41,6 +41,7 @@ router.post("/", (req, res) => {
           res.json({
             token,
             user: {
+              _id: user._id,
               name: user.name,
               email: user.email,
               password: user.password

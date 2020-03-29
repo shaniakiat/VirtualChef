ff7288f3127d24e9a796fcd35bc41c245262f3ae
david <luongd5@student.lasalle.edu>
2020-03-29 14:21:09 -0400

    tables

--- client/src/components/Globals/Restaurant/DisplayRestaurant.js
+++ client/src/components/Globals/Restaurant/DisplayRestaurant.js
@@ -1,13 +1,37 @@
 import React, { useState, useInput, useEffect } from "react";
+import { Route, Redirect } from "react-router-dom";
+import "../../Styles/Table.css";
 
 const DisplayRestaurant = ({ restaurantData }) => {
   return (
     <div>
-      {restaurantData.map((obj, i) => (
-        <div className="grid-food">
-          <li key={obj[1]}>{obj.name}</li>
-        </div>
-      ))}
+      <table className="table">
+        <thead>
+          <tr className="row100   head">
+            <th className="column100 column1">Restaurant Name</th>
+            <th className="column100 column2">Rating</th>
+            <th className="column100 column3">Price ($)</th>
+            <th className="column100 column4">Phone Number</th>
+            <th className="column100 column5">Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {restaurantData.map((obj, i) => (
+            <div>
+              {/* <li key={obj[1]}>{obj.name}</li> */}
+
+              <td className="row100">{obj.name}</td>
+              <td className="row100">{obj.rating}</td>
+              <td className="row100">{obj.price}</td>
+              <td className="row100">{obj.display_phone}</td>
+              <td className="row100">
+                {obj.location.display_address[0] +
+                  obj.location.display_address[1]}
+              </td>
+            </div>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };

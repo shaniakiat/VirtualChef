feada8bbe2445062fcb815283e4a2801f6cbca9d
shaniakiat <kiat.shania@gmail.com>
2020-04-13 18:14:11 -0400

    fixing routing

---
+++ client/src/components/Globals/Documentation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./../Styles/Documentation.css";
+
+const Documentation = () => {
+  return (
+    <div className="documentation-page">
+      <h1>Hi David 😊</h1>
+      <h3>I hope you have a good day!</h3>
+    </div>
+  );
+};
+
+export default Documentation;

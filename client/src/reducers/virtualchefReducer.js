7fb55481cb46bd2b9cec26ea335e4a4d2c8bd1be
david <luongd5@student.lasalle.edu>
2020-04-17 00:09:24 -0400

    deleted unused reducers

--- client/src/reducers/virtualchefReducer.js
+++ client/src/reducers/virtualchefReducer.js
@@ -1,8 +1,7 @@
-import { CREATEKEY, PREDICT } from "../actions/types";
+import { CREATEKEY } from "../actions/types";
 
 const initialState = {
   keys: [],
-  data: [],
   loading: false,
 };
 export default function (state = initialState, action) {
@@ -13,13 +12,6 @@ export default function (state = initialState, action) {
         keys: action.payload,
         loading: false,
       };
-    case PREDICT:
-      return {
-        ...state,
-        data: action.payload,
-        loading: false,
-      };
-
     default:
       return state;
   }

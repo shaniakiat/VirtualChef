a02589d73ab38e9aea5c0b7aba3b62e095e18151
shaniakiat <kiat.shania@gmail.com>
2020-04-18 00:00:38 -0400

    react-multi-carousel

--- client/src/components/Globals/Predictions/IngredientList.js
+++ client/src/components/Globals/Predictions/IngredientList.js
@@ -1,9 +1,33 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import UAParser from "ua-parser-js";
+import Fade from "react-reveal/Fade";
+
+import Carousel from "react-multi-carousel";
+
 import "../../Styles/Ingredients.css";
 import * as d3 from "d3";
-function IngredientList({ match }) {
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+    paritialVisibilityGutter: 0,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+    paritialVisibilityGutter: 0,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    paritialVisibilityGutter: 0,
+  },
+};
+
+function IngredientList({ match, deviceType }) {
   /* -------------- INGREDIENTS -----------------*/
   const [ingredient, setIngredient] = useState([]);
   const [id, setId] = useState("");
@@ -29,7 +53,7 @@ function IngredientList({ match }) {
           .replace(/-/g, "+")
           .toLowerCase()}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
       )
-      .then(res => {
+      .then((res) => {
         // SET INGREDIENTS
         setIngredient(res.data.hits);
 
@@ -38,13 +62,13 @@ function IngredientList({ match }) {
         setNutrition(res.data.hits[0].recipe.totalNutrients);
         getNutritionData(param);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, [getNutritionData, match.params.id]);
   console.log(nutrition);
 
-  getNutritionData = params => {
+  getNutritionData = (params) => {
     console.log("in the getNutritionData function");
     let nutritiontypearray = [];
     let quantityarr = [];
@@ -52,12 +76,12 @@ function IngredientList({ match }) {
     let data = [];
     //let data: { labels: string; quantities: number}[] = [];
     const filteredNutritionObj = Object.keys(params)
-      .filter(key => keysAllowed.includes(key))
+      .filter((key) => keysAllowed.includes(key))
       .reduce((obj, key) => {
         obj[key] = params[key];
         return obj;
       }, {});
-    keysAllowed.map(i => {
+    keysAllowed.map((i) => {
       nutritiontypearray.push(filteredNutritionObj[i].label);
       quantityarr.push(
         Math.round(filteredNutritionObj[i].quantity * 100) / 100
@@ -74,14 +98,11 @@ function IngredientList({ match }) {
 
   function drawSvg(data, unit) {
     //d3js
-    const margin = { top: 40, right: 40, bottom: 40, left: 50 };
-    const width = 380 - margin.left - margin.right;
+    const margin = { top: 40, right: 50, bottom: 40, left: 60 };
+    const width = 350 - margin.left - margin.right;
     const height = 300 - margin.top - margin.bottom;
     // set the ranges for the graph
-    const x = d3
-      .scaleBand()
-      .range([0, width])
-      .padding(0.3);
+    const x = d3.scaleBand().range([0, width]).padding(0.3);
 
     // const xScale = d3
     //   .scale()
@@ -106,7 +127,7 @@ function IngredientList({ match }) {
       .attr("class", "graph");
 
     container
-      .append("h3")
+      .append("p")
       .text(`Nutritional Values`)
       .attr("class", "ingredient-h3");
     // append the svg object to the body of the page
@@ -134,10 +155,7 @@ function IngredientList({ match }) {
       .attr("transform", "translate(0," + height + ")")
       .attr("class", "x-axis")
       .call(d3.axisBottom(y));
-    svg
-      .append("g")
-      .attr("class", "y-axis")
-      .call(d3.axisLeft(y));
+    svg.append("g").attr("class", "y-axis").call(d3.axisLeft(y));
 
     update(data, unit, margin, width, height, x, y, container, svg, tip);
   }
@@ -157,7 +175,7 @@ function IngredientList({ match }) {
     // Scale the range of the data in the x axis
     // console.log(data.labels);
     x.domain(
-      data.map(d => {
+      data.map((d) => {
         console.log(d.labels);
         return d.labels;
       })
@@ -167,9 +185,9 @@ function IngredientList({ match }) {
     console.log(data.quantities);
     y.domain([
       0,
-      d3.max(data, d => {
+      d3.max(data, (d) => {
         return d.quantities;
-      })
+      }),
     ]);
     // Select all bars on the graph, take them out, and exit the previous data set.
     // Enter the new data and append the rectangles for each object in the poll array
@@ -180,21 +198,21 @@ function IngredientList({ match }) {
       .append("rect")
       .attr("class", "bar")
       .attr("fill", "#196d4c")
-      .attr("x", d => {
+      .attr("x", (d) => {
         return x(d.labels);
       })
       .attr("width", x.bandwidth())
-      .attr("y", d => {
+      .attr("y", (d) => {
         return y(d.quantities);
       })
-      .attr("height", d => {
+      .attr("height", (d) => {
         return height - y(d.quantities);
       })
-      .on("mousemove", d => {
+      .on("mousemove", (d) => {
         tip
           .style("position", "absolute")
-          .style("left", `${d3.event.pageX + 10}px`)
-          .style("top", `${d3.event.pageY + 20}px`)
+          .style("left", `${d3.event.pageX + 20}px`)
+          .style("top", `${d3.event.pageY - 60}px`)
           .style("display", "inline-block")
           .style("opacity", "1")
           .html(
@@ -218,7 +236,7 @@ function IngredientList({ match }) {
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left)
       .attr("x", 0 - height / 2)
-      .attr("dy", "1em")
+      .attr("dy", "0.8em")
       .style("text-anchor", "middle")
       .text("Grams (g)");
 
@@ -232,29 +250,62 @@ function IngredientList({ match }) {
   return (
     <div className="ingredient-list">
       {/* <h3 className="ingredient-h3">You have clicked on </h3> */}
-      <h3 className="ingredient-h3-id">{id.toLocaleUpperCase()}</h3>
-      <div className="d3-bar-chart"></div>
-      {/* <div>⬅️</div> */}
-      <ul className="recipes">
-        <div className="grid-container">
-          {ingredient.map(obj => (
+      <Fade up delay={50}>
+        <h2 className="ingredient-h3-id">{id.toLocaleUpperCase()}</h2>
+        <div className="d3-bar-chart"></div>
+      </Fade>
+      <Fade up delay={100}>
+        <Carousel
+          ssr={true}
+          partialVisbile
+          swipeable={true}
+          deviceType={deviceType}
+          responsive={responsive}
+          keyBoardControl={true}
+          /* dotListClass="custom-dot-list-style"
+          removeArrowOnDeviceType={["tablet", "mobile"]} */
+        >
+          {/* <div className="grid-container"> */}
+          {ingredient.map((obj) => (
             <div className="grid-item">
-              <img className="ingredient-img" src={obj.recipe.image}></img>
-              <li>
-                <h3 className="label">{obj.recipe.label.toLowerCase()}</h3>
-                <ul>
-                  {obj.recipe.ingredientLines.map(ngrdnt => (
-                    <li className="ingredients">{ngrdnt.toLowerCase()}</li>
-                  ))}
-                </ul>
-              </li>
+              <div className="list-container">
+                <div className="list-grid-img">
+                  <img className="ingredient-img" src={obj.recipe.image}></img>
+                </div>
+                <div className="list-grid-ings">
+                  <li>
+                    <h3 className="label">{obj.recipe.label}</h3>
+
+                    <p className="ingredients-p">Ingredients</p>
+                    <ul className="ingredients-ul">
+                      {obj.recipe.ingredientLines.map((ngrdnt) => (
+                        <li className="ingredients">{ngrdnt.toLowerCase()}</li>
+                      ))}
+                    </ul>
+                  </li>
+                </div>
+              </div>
             </div>
           ))}
-        </div>
-      </ul>
-      {/* <div>➡️</div> */}
+          {/* </div> */}
+        </Carousel>
+      </Fade>
     </div>
   );
 }
+
+IngredientList.getInitialProps = ({ req }) => {
+  let userAgent;
+  if (req) {
+    userAgent = req.headers["user-agent"];
+  } else {
+    userAgent = navigator.userAgent;
+  }
+  const parser = new UAParser();
+  parser.setUA(userAgent);
+  const result = parser.getResult();
+  const deviceType = (result.device && result.device.type) || "desktop";
+  return { deviceType };
+};
 
 export default IngredientList;

55f6fa6e3b1685b008a733b1f0dd1e86a5755cfc
david <luongd5@student.lasalle.edu>
2020-04-27 23:47:40 -0400

    docu, stuff we might want to display

--- client/src/components/Globals/Documentation.js
+++ client/src/components/Globals/Documentation.js
@@ -26,6 +26,16 @@ const Documentation = (props) => {
     console.log("create new key function finished");
   };
 
+  const fetchExample = `fetch(https://floating-plains-35923.herokuapp.com/prediction/:id", {
+    headers: {
+      Authorization: "Bearer key",
+    },
+  })
+    .then((res) => res.json())
+    .then(function (data) {
+      console.log(data); //expecting array
+      res.status(200).send({ data });
+    });`;
   return (
     <div className="documentation-page">
       {/* if auth.user has apikey, then show your api key, else, show the button */}
@@ -33,11 +43,38 @@ const Documentation = (props) => {
       <p>
         Welcome to <strong>Virtual Chef API intergration guide!</strong>
       </p>
+      <p>
+        Virtual Chef provides helpful tools for developers. Explore our neural
+        network that we use to make your food prediction.
+      </p>
 
       <button type="button" className="button" onClick={createNewKey}>
         Get API Key
       </button>
       <p>Your API key will show up in the bottom of your profile.</p>
+
+      <p>
+        Virtual Chefuses API keys to authenticate requests, please request one
+        if you don't have one{" "}
+      </p>
+      <p>
+        Your API keys carry many privileges, so be sure to keep them secure! Do
+        not share your secret API keys in publicly accessible areas such as
+        GitHub, client-side code, and so forth.
+      </p>
+
+      <p>
+        Base URL for prediction:
+        https://floating-plains-35923.herokuapp.com/prediction
+      </p>
+      <p>${fetchExample}</p>
+
+      <p>":id" should be the food you are searching</p>
+
+      <p>
+        Finally, the reponse will be ten foods or ingredients with the accuracy
+        percentage from the neural network.
+      </p>
     </div>
   );
 };
